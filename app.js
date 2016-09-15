645e0dd03e636e542aa8b788d0059eec162ce971
Jacob1233 <jwilson1233@gmail.com>
2016-09-14 19:46:45 -0500

    broadcast to only the socket that calls the request

--- app.js
+++ app.js
@@ -15,27 +15,27 @@ app.get('/', function (req, res) {
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
-let CLIENT_ID = '736807524112-fl28n29p67sl10edkkoll272go41l5kr.apps.googleusercontent.com';
+let CLIENT_ID = '736807524112-fl28n29p67sl10edkkoll272go41l5kr.apps.googleusercontent.com'; // google app client id
 
 io.on('connection', function (socket) {
 
     socket.on('verify id_token', function (id_token) {
-        verifyToken(id_token);
+        verifyToken(id_token, socket);
     });
 
 });
 
-function verifyToken(id_token) {
+function verifyToken(id_token, socket) {
     request(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${id_token}`, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var js = JSON.parse(body);
             var aud = js.aud;
             if (verifyClientID(aud) == false) {
-                var msg = `Whoops. Something went wrong.`;
-                io.emit('message', msg);
+                var msg = `Error. Security cannot be established.`;
+                socket.emit('message', msg);
             } else {
                 var msg = `${js.sub} successfully signed in`;
-                io.emit('message', msg);
+                socket.emit('message', msg);
             }
         }
     });
