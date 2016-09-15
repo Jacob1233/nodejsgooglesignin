1094e68666749df4b135f358f8cf272037737177
Jacob1233 <jwilson1233@gmail.com>
2016-09-14 19:23:41 -0500

    changed project name, v1.0.0

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+var request = require('request');
 var express = require('express');
 var app = require('express')();
 var http = require('http');
@@ -10,40 +12,37 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/protected', function (req, res) {
-    res.sendFile(__dirname + '/protected/index.html');
-});
-
-
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use('/public-assets', express.static(__dirname + '/public/assets/'));
-app.use('/protected-assets', express.static(__dirname + '/protected/assets/'));
+app.use('/assets', express.static(__dirname + '/public/assets/'));
 
-socket.on('verify id_token', function(id_token){
-    verifyToken(id_token);
-  });
+let CLIENT_ID = '736807524112-fl28n29p67sl10edkkoll272go41l5kr.apps.googleusercontent.com';
+
+io.on('connection', function (socket) {
+
+    socket.on('verify id_token', function (id_token) {
+        verifyToken(id_token);
+    });
+
+});
 
 function verifyToken(id_token) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${id_token}`);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function () {
-        // console.log(xhr.responseText);
-
-        var js = JSON.parse(xhr.responseText);
-        var aud = js.aud;
-        if (verifyClientID(aud) == false){
-            console.log('client_id could not be verified')
+    request(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${id_token}`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var js = JSON.parse(body);
+            var aud = js.aud;
+            if (verifyClientID(aud) == false) {
+                var msg = `${aud} could not be verfied`;
+                io.emit('message', msg);
+            }
+            var msg = `${js.sub} successfully signed in`;
+            io.emit('message', msg);
         }
-    };
-    xhr.send('idtoken=' + id_token);
+    });
 }
 
 function verifyClientID(aud) {
-    var client_id = '736807524112-fl28n29p67sl10edkkoll272go41l5kr.apps.googleusercontent.com';
-
-    if (aud == client_id) {
+    if (aud == CLIENT_ID) {
         return true;
     }
     return false;
